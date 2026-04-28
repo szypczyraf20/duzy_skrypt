7bf42db402a118cf078a7f051d3edff7ea3d02d2
szypczyraf20 <szypczyraf15@gmail.com>
2026-04-28 22:56:36 +0200

    Pierwsze zmiany. Nic użytecznego.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # duzy_skrypt
-Ale głupie by robić repozytorium do takiego małego projektu. XD
+Ale głupie by robić repozytorium do takiego małego projektu. XD   
+I jeszcze muszę walnąć 4 commity do tego czegoś. Haha.   
+Ale nie będę czatować.   
+Zrobię uczciwie.
---
+++ skrypt.sh
@@ -0,0 +1,16 @@
+# Author           : Rafał Szypczyński, s208477@student.pg.edu.pl
+# Created On       : 28 IV 2026
+# Last Modified On : date 
+# Version          : 1
+#
+# Description      :
+# Duży skrypt Rafała Szypczyńskiego. Temat: Analiza plików dziennika
+#
+# Licensed under GPL (see /usr/share/common-licenses/GPL for more details
+# or contact # the Free Software Foundation for a copy)
+# 
+# Generative AI statement (keep ONE line below, delete the others):
+# * I did NOT use GenAI tools while developing this code.
+# * I used GenAI tools for support only (e.g., explanations, debugging, small snippets).
+# * I used GenAI tools to generate a significant portion of this code (>50%).
+cat /var/log/syslog | grep "rafal"
