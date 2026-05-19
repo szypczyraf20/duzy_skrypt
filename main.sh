3bd0fb3caf475113960f1a62de1c30807e98b9d7
szypczyraf20 <szypczyraf15@gmail.com>
2026-05-19 14:03:37 +0200

    Zacznij wprowadzać możliwość używania parametrów

--- main.sh
+++ main.sh
@@ -14,14 +14,61 @@
 # * I did NOT use GenAI tools while developing this code.
 
 #!/bin/bash
-ANALYZED_LOGS=($@)
-
-# Dodaj plik konkatenujący wyjście kilku poleceń ./concatenation.sh
-PATTERN=`cat config.txt`
-for ANALYZED_LOG in ${ANALYZED_LOGS[@]}; do
-    PARAMETERS=`./log_name_translator.sh $ANALYZED_LOG`
-    ./concatenation.sh $PARAMETERS | grep -E "$PATTERN" 
-    MATCHED=`./concatenation.sh $PARAMETERS | grep -E "$PATTERN" | wc -l` 
+while getopts "vh" OPCJE; do
+VERBOSE="OFF"
+  case $OPCJE in
+    v) 
+        VERBOSE="ON"
+        ;;
+    h)
+        echo "SPOSÓB UŻYCIA: " 
+        echo "./skrypt.sh [OPCJE] [POLECENIE]"
+        echo
+        echo "OPCJE"
+        echo "-h  Wypisz tą wiadomość"
+        echo "-v  Tryb gadatliwy (verbose)"
+        echo
+        echo "POLECENIA"
+        echo "n  Najdłuższe sesje"
+        echo "u  Liczba aplikacji powodujących zdarzenia w pliku syslog"
+        echo "z  Obecnie zalogowani użytkownicy"
+        echo "f  Nieudane logowania"
+        exit
+        ;;
+  esac
 done
 
+INPUT_PARAMS=($@)
+echo $INPUT_PARAMS
+
+for PARAM in $INPUT_PARAMS; do
+    if [ "${PARAM::1}" = "-" ]; then
+        continue
+    else
+        ANALYZED_LOG+=$PARAM
+    fi
+done
+
+# Obraca się po kolejnych linijkach pliku config.txt
+while IFS= read -r PATTERN; do
+MATCHED=0
+for ANALYZED_LOG in ${ANALYZED_LOGS[@]}; do
+    # Tworzy parametry do funkcji ./concatenation.sh
+    PARAMETERS=`./log_name_translator.sh $ANALYZED_LOG`
+
+    # Wypisuje dopasowane linie
+    if [ "$VERBOSE" = "ON" ]; then
+        ./concatenation.sh $PARAMETERS | grep -E "$PATTERN" 
+    fi
+
+    # Oblicza ilość dopasowań z obecnego logu
+    CURRENTLY_MATCHED=`./concatenation.sh $PARAMETERS | grep -E "$PATTERN" | wc -l` 
+
+    # Sumuje ilość dopasowań
+    MATCHED=$((MATCHED+CURRENTLY_MATCHED))
+
+# Wypisuje wynik
 echo "Liczba dopasowań: $MATCHED"
+
+done
+done < "config.txt"
